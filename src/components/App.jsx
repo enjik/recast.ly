21175ca7f25cc120c2015127a1c8cb982d948310
enjik <enjikim17@gmail.com>
2018-06-27 20:09:47 -0700

    passed specs up to searchView

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,20 +1,42 @@
-var App = () => (
-  <div>
-    <nav className="navbar">
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      currentVideo: window.exampleVideoData[0],
+      videos: window.exampleVideoData
+    }
+  }
+
+  render() {
+  
+    return (
+    <div>
+      <nav className="navbar">
       <div className="col-md-6 offset-md-3">
-        <div><h5><em>search</em> view goes here</h5></div>
+        <Search onClick={window.searchYouTube}/>
       </div>
-    </nav>
-    <div className="row">
-      <div className="col-md-7">
-        <div><h5><em>videoPlayer</em> view goes here</h5></div>
-      </div>
-      <div className="col-md-5">
-        <div><h5><em>videoList</em> view goes here</h5></div>
+      </nav>
+      <div className="row">
+        <div className="col-md-7">
+          <VideoPlayer video={this.state.currentVideo}/>
+        </div>
+        <div className="col-md-5">
+          <VideoList videos={this.state.videos} onClick={this.handleClick.bind(this)}/>
+        </div>
       </div>
     </div>
-  </div>
-);
+    )
+
+  }
+  
+  handleClick(video) {
+    this.setState({currentVideo: video});
+    // console.log(this);
+    // console.log(this.state.currentVideo);
+
+  }
+};
 
 // In the ES6 spec, files are "modules" and do not share a top-level scope
 // `var` declarations will only exist globally where explicitly defined
--- src/lib/searchYouTube.js
+++ src/lib/searchYouTube.js
@@ -1,5 +1,14 @@
 var searchYouTube = (options, callback) => {
-  // TODO
+  console.log('hello');
+  $.ajax({
+    url: 'https://www.googleapis.com/youtube/v3/search',
+    method: 'GET',
+    data: options,
+    success: callback,
+    error: function() {
+      console.log('your request failed');
+    }
+  })
 };
 
 window.searchYouTube = searchYouTube;
